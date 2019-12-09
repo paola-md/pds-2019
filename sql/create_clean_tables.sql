60d3c66b9ff0ed4f5d586da9e52b50ceab8abe88
paola-md <paola.domen@gmail.com>
2019-12-09 15:56:56 -0600

    cambios

--- sql/create_clean_tables.sql
+++ sql/create_clean_tables.sql
@@ -10,10 +10,10 @@ create table cleaned.artists as (
 		"Nationality" as nationality,
 		case when "Gender" = 'Male' 
 		then 
-			'M' 
+			0 
 		else 
-			'F' 
-		end as gender,
+			1 
+		end as female,
 		case when "BeginDate"::int = 0 
 		then 
 			NULL 
@@ -26,8 +26,15 @@ create table cleaned.artists as (
 		else
 			to_date("EndDate", 'YYYY') 
 		end as death,
-		--daterange(birth, death) as lifespan,
-		"Wiki QID" as wiki,
+		daterange(
+			case when "BeginDate"::int = 0 then NULL 
+			else to_date("BeginDate", 'YYYY')
+			,
+			case when "EndDate"::int = 0 then NULL 
+			else to_date("EndDate", 'YYYY') 
+			) 
+		as lifespan,
+		lower("Wiki QID") as wiki,
 		"ULAN"::int as ulan
 	from raw.artists	
 );
@@ -39,7 +46,7 @@ create index cleaned_artists_artist_ix on cleaned.artists (artist);
 drop table if exists cleaned.artworks;
 create table cleaned.artworks as (
 	select 
-		"Title" as title,
+		lower("Title") as title,
 		string_to_array("ConstituentID",', ')::int[] as artist,
 		case when substring("Date" from '\d{4}')::int = NULL
 		then 
@@ -47,20 +54,26 @@ create table cleaned.artworks as (
 		else
 			to_date(substring("Date" from '\d{4}'),	'YYYY')
 		end as  creation,
-		"Medium" as medium,
-		"CreditLine" as creditline,
-		"AccessionNumber" accession,
-		"Classification" as classification,
+		lower("Medium") as medium,
+		lower("CreditLine") as creditline,
+		"AccessionNumber" as accession,
+		lower("Classification") as classification,
 		to_date("DateAcquired",'YYYY-MM-DD') as acquisition,
-		"Cataloged" as cataloged,
-		"ObjectID" as artwork,
+		case when "Cataloged" = 'Y' 
+		then 
+			1 
+		else 
+			0 
+		end as cataloged,
+		lower("Department") as department,
+		"ObjectID"::int as artwork,
 		"URL" as url,
 		"ThumbnailURL" as thumb_url,
 		"Diameter (cm)"::numeric(10,2) as diameter,
 		"Depth (cm)"::numeric(10,2) as depth, 
 		"Height (cm)"::numeric(10,2) as height,
 		"Width (cm)"::numeric(10,2) as width,
-		"Duration (sec.)"::numeric(10,2) as duration
+		"Duration (sec.)"::int as duration
 	from raw.artworks		
 );
 
