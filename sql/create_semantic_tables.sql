cc76c428df244d4578076bdc727c7c25a05dec0f
paola-md <paola.domen@gmail.com>
2019-12-09 16:36:42 -0600

    cambios

---
+++ sql/create_semantic_tables.sql
@@ -0,0 +1,34 @@
+create schema if not exists semantic;
+
+-- Entidad son las clasificaciones
+drop table if exists semantic.entities cascade;
+create table semantic.entities as (
+	select distinct
+		clasification,
+		department
+		from 
+			cleaned.artworks
+);
+
+-- Suponemos que un evento es que llegué una obra al museo
+drop table if exists semantic.events cascade;
+create table semantic.events as (
+	select
+		title,
+		artist,
+		creation,
+		medium,
+		creditline,
+		classification,
+		acquisition,
+		cataloged,
+		department,
+		artwork,
+		diameter,
+		depth, 
+		height,
+		width,
+		duration
+	from 
+	cleaned.artworks
+);
