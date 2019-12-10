8699f52c5777250f703ad1f5e62d06cf91fe3ea2
paola-md <paola.domen@gmail.com>
2019-12-09 20:50:32 -0600

    cambios

---
+++ sql/create_features.sql
@@ -0,0 +1,27 @@
+create schema if not exists features;
+
+drop table if exists features.aggregated;
+
+
+create table if not exists features.aggregated as (
+select * from
+(
+  select
+    as_of_date,
+    artwork
+    from
+        labels.classified_department
+) as aod
+             left join lateral ( -- for loop
+               select
+                 count(*) filter(where daterange((aod.as_of_date - interval '2 year')::date, aod.as_of_date::date) @> acquisition) as "COUNT(*, @1M)",
+                 count(*) filter(where daterange((aod.as_of_date - interval '3 year')::date, aod.as_of_date::date) @> acquisition) as "COUNT(*, @3M)",
+                 count(*) as "COUNT(*)"
+                 from semantic.events
+                where aod.artwork = artwork
+             ) as t2
+                 on true
+);
+create index features_aggregated_as_of_date_ix on features.aggregated(as_of_date);
+create index features_aggregated_artwork_ix on features.aggregated(artwork);
+create index features_aggregated_artwork_as_of_date_ix on features.aggregated(artwork, as_of_date);
