bc27f69e7a05190e3ba5e636930252b046f6284f
paola-md <paola.domen@gmail.com>
2019-12-10 16:18:53 -0600

    add scripts

---
+++ external_code/run_sql.sh
@@ -0,0 +1,25 @@
+cd moma
+
+./moma.py create-schemas
+echo "Schemas created"
+
+./moma.py create-raw-tables
+echo "Raw tables greated"
+
+./moma.py load-moma
+echo "Data Loaded"
+
+./moma.py create_clean_tables
+echo "Clean tables created"
+
+./moma.py create_semantic_tables
+echo "Semantic tables created"
+
+./moma.py create-cohort
+echo "Cohort created"
+
+./moma.py create-labels
+echo "Labels created"
+
+#./moma.py create-features
+echo "Features created"
