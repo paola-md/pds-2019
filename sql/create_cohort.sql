51bf02f2f700e1ca4dfa903372ac59c5c6f2d52c
paola-md <paola.domen@gmail.com>
2019-12-09 19:59:54 -0600

    cambios

---
+++ sql/create_cohort.sql
@@ -0,0 +1,50 @@
+drop schema if exists cohorts cascade; 
+create schema if not exists cohorts;
+
+drop table if exists cohorts.new_arrivals;
+
+create table if not exists cohorts.new_arrivals as (
+with as_of_dates as (
+  select
+    generate_series(min(acquisition), max(acquisition), '10 years') as as_of_date
+    from
+        semantic.events
+),
+
+new_artwork as(
+	select
+		title,
+		artist,
+		creation,
+		medium,
+		creditline,
+		classification,
+		acquisition,
+		cataloged,
+		artwork,
+		diameter,
+		depth, 
+		height,
+		width,
+		duration,
+		
+		aod.as_of_date::date,
+		daterange(
+		(aod.as_of_date - interval '20 year')::date,
+		aod.as_of_date::date)
+		@> acquisition as "new?"
+	from (
+		select
+		as_of_date
+		from as_of_dates
+		) as aod
+		left join lateral(
+			select *, age(acquisition, as_of_date)
+			from semantic.events
+		) as t2 on true
+)
+
+select * from
+new_artwork
+where "new?" is true 
+)
