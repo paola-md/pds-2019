b388b10ca567bc64d5da76b80bf1114ebe566924
paola-md <paola.domen@gmail.com>
2019-12-11 14:53:52 -0600

    name change

--- sql/create_joined_table.sql
+++ sql/create_joined_table.sql
@@ -1,8 +1,8 @@
 create schema if not exists finals;
 
-drop table if exists finals.artworks cascade;
+drop table if exists finals.classify_photographs cascade;
 
-create table if not exists finals.artworks as (
+create table if not exists finals.classify_photographs as (
 	select * from labels.classified_department labs
 	join features.aggregated feat
 	on labs.artwork = feat.artwork
