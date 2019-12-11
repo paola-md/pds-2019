fe17eb86d584d061c3d803b0b98f60a32f666705
paola-md <paola.domen@gmail.com>
2019-12-11 15:05:07 -0600

    clasi

--- sql/create_joined_table.sql
+++ sql/create_joined_table.sql
@@ -5,5 +5,5 @@ drop table if exists finals.classify_photographs cascade;
 create table if not exists finals.classify_photographs as (
 	select * from labels.classified_department labs
 	join features.aggregated feat
-	on labs.artwork = feat.artwork
+	on labs.classification = feat.classification
 );
