bebd6e535d448933c1d9b53307e1013ff86a47f1
paola-md <paola.domen@gmail.com>
2019-12-10 22:04:30 -0600

    mas comentarios

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# """
+# La función de este script es crear
+# un usuario para la base de datos
+# y la base moma
+# """
+
 # Creación de credenciales: usuario y contraseña
 sudo -i -u postgres psql -c "CREATE USER moma with PASSWORD 'marmol';" 
 
