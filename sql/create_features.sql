0ed85dc9dafebcbf9e6aefee114e186b5509c34f
paola-md <paola.domen@gmail.com>
2019-12-11 11:38:56 -0600

    nuevas feat

--- sql/create_features.sql
+++ sql/create_features.sql
@@ -3,26 +3,25 @@ create schema if not exists features;
 drop table if exists features.aggregated;
 
 
-create table if not exists features.aggregated as (
-select * from
-(
-  select
-    as_of_date,
-    artwork
-    from
-        labels.classified_department
-) as aod
-             left join lateral ( 
-               select
-                  count(distinct artwork) filter(where daterange((aod.as_of_date - interval '5 year')::date, aod.as_of_date::date) @> acquisition) as "artwork_5y",
-                  avg(height) mean_heigth
-				  , min(height) min_heigth
-				  , count(*) obras
+create table if not exists features.aggregated as 
+			  (select 
+					classification,
+					count(distinct artworks) artworks
+					, min(height) min_heigth
+			        , avg(height) mean_heigth
+				    , max(height) max_heigth
+					, min(diameter) min_diameter
+					, avg(diameter) mean_diameter
+					, max(diameter) max_diameter
+					, min(width) min_widht
+					, avg(width) mean_width
+					, max(width) max_width
+					, min(duration) min_duration
+					, avg(duration) avg_duration
+					, max(duration) max_duration
+				    , count(*) obras
 				  from semantic.events
-                where aod.artwork = artwork
-             ) as t2
-                 on true
-);
-create index features_aggregated_as_of_date_ix on features.aggregated(as_of_date);
+               group by classification ) t1
+			             
 create index features_aggregated_artwork_ix on features.aggregated(artwork);
-create index features_aggregated_artwork_as_of_date_ix on features.aggregated(artwork, as_of_date);
+
