aa507a608ff3917d1710a7e7c718b5e846615a69
paola-md <paola.domen@gmail.com>
2019-12-10 21:52:27 -0600

    permisos

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Cambiamos al usuario postgres y creamos credenciales
-sudo su postgres 
-
 # Creación de credenciales: usuario y contraseña
-psql -c "CREATE USER moma2 with PASSWORD 'marmol';" 
-
+sudo -i -u postgres psql -c "CREATE USER moma2 with PASSWORD 'marmol';" 
 
 # Creación de BD 
-psql -c "CREATE DATABASE moma2 OWNER moma2;"
+sudo -i -u postgres psql -c "CREATE DATABASE moma2 OWNER moma2;"
