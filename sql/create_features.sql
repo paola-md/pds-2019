19d65f2fc8e8489258baf994a56f952a268eb5e5
paola-md <paola.domen@gmail.com>
2019-12-11 16:50:10 -0600

    mas meses

--- sql/create_features.sql
+++ sql/create_features.sql
@@ -9,7 +9,9 @@ create table if not exists features.aggregated as
 					left join lateral (
 										select
 										count(*) filter (where daterange((aod.as_of_date - interval '2 month'):: date, aod.as_of_date::date) @>acquisition) as "COUNT(2 meses)"
-										,count(*) filter (where daterange((aod.as_of_date - interval '8 month'):: date,aod.as_of_date::date) @>acquisition) as "COUNT(4 meses)"
+										,count(*) filter (where daterange((aod.as_of_date - interval '4 month'):: date,aod.as_of_date::date) @>acquisition) as "COUNT(4 meses)"
+										,count(*) filter (where daterange((aod.as_of_date - interval '8 month'):: date,aod.as_of_date::date) @>acquisition) as "COUNT(8 meses)"
+										,count(*) filter (where daterange((aod.as_of_date - interval '16 month'):: date,aod.as_of_date::date) @>acquisition) as "COUNT(16 meses)"										
 										,count(*) as "totals"
 										from semantic.events where aod.classification=classification) 
 										as t2 on true
